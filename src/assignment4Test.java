d0f4d12784bb61c3b55ffe5b0a860ac018d7890c
albertcarrete <albertcarrete@gmail.com>
2016-05-05 12:27:39 -0700

    Base Push

--- src/assignment4Test.java
+++ src/assignment4Test.java
@@ -1,8 +1,39 @@
+/*
+ * ================================================
+ * Author: 		Albert Carrete
+ * Class: 		CSC-18C
+ * Project: 	Assignment 5 
+ * ================================================
+ * Eratosthenes With Queues
+ * 
+ * 1. 	Create a queue named queueOfIntegers, enqueue 
+ * 		it with consecutive integers 2 through n.
+ * 2. 	Create an empty queue to store primes, perhaps 
+ * 		named queueOfPrimes.
+ * 3. 	Do
+ * 4. 	Get the next prime number, p, by removing the 
+ * 		first value in queueOfNumbers.
+ * 5. 	Enqueue the value of p into queue of primes. 
+ * 6. 	Create a new queue and fill this queue by doing 
+ * 		the following:
+ * 			- While queueOfNumbers is not empty, dequeue
+ * 			the front number, if it is not divisible by
+ * 			p then enequeue this number to newQueue 
+ * 			(queueOfPrimes.back() is our current prime
+ * 			number)
+ * 8.	Assign the queueOfNumbers object with the newly created queue object
+ * 9. 	While (queueOfPrimes.back() < sqrt(n))
+ * 10.	Display the primes, which are all the values in the queueOfPrimes object.
+ * */
+
 
 public class assignment4Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+
+		Queue queueOfIntegers = new Queue();
+		
 
 	}
 
